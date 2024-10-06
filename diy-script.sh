455aa15869e527347cc52372938072dd6c3e2c44
washsky <111639029+washsky@users.noreply.github.com>
2024-10-06 15:25:55 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -30,6 +30,7 @@ function git_sparse_clone() {
 }
 
 # 添加额外插件
+git clone --depth=1 -b main https://github.com/VIKINGYFY/luci-app-wolplus package/luci-app-wolplus
 #git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 #git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
 #git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
@@ -86,7 +87,7 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/l
 
 # iStore
 #git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-git_sparse_clone main https://github.com/linkease/istore luci-app-store
+#git_sparse_clone main https://github.com/linkease/istore luci-app-store
 
 # 在线用户
 git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
